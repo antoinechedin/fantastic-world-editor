b3c3620504f687f1631be43c52a791ca79e2fbf1
antoinechedin <antoine.chedin@gmail.com>
2022-07-17 12:37:24 +0200

    Add Leaflet style and script

--- fantastic-world-editor.php
+++ fantastic-world-editor.php
@@ -17,6 +17,10 @@ class FantasticWorldEditor {
 
 	public function __construct() {
 		add_action( 'init', array( $this, 'customPostLocationRegister' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'publicEnqueue' ) );
+
+		add_filter( 'style_loader_tag', array( $this, 'addStyleAttributes' ), 10, 2 );
+		add_filter( 'script_loader_tag', array( $this, 'addScriptAttributes' ), 10, 3 );
 	}
 
 	public function customPostLocationRegister() {
@@ -38,6 +42,27 @@ class FantasticWorldEditor {
 				),
 			)
 		);
+	}
+
+	public function publicEnqueue() {
+		wp_enqueue_style( 'leaflet', 'https://unpkg.com/leaflet@1.8.0/dist/leaflet.css', array(), null);
+		wp_enqueue_script( 'leaflet', 'https://unpkg.com/leaflet@1.8.0/dist/leaflet.js', array(), null );
+	}
+
+	public function addStyleAttributes( $html, $handle ) {
+		if ( 'leaflet' === $handle ) {
+			return str_replace( 'media="all"', 'integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin=""', $html );
+		}
+
+		return $html;
+	}
+
+	public function addScriptAttributes( $tag, $handle, $src ) {
+		if ( 'leaflet' === $handle ) {
+			return '<script id="' . $handle . '-js" src="' . $src . '" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>' . "\n";
+		}
+
+		return $tag;
 	}
 }
 
